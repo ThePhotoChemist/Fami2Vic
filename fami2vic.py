a0649cdb414332234665a08b8ddb3993c98c4b4d
ThePhotoChemist <jonathan.hilty@gmail.com>
2021-02-18 19:47:39 -0500

    Added support for noise channel (percussion).

--- fami2vic.py
+++ fami2vic.py
@@ -17,12 +17,12 @@ DataStartAddrDec=int(DataStartAddress,16)
 
 SwapS2S3=0
 
-Square1NoteModifier=-12
+Square1NoteModifier=-24
 Square2NoteModifier=-12
 TriangleNoteModifier=-12
 
 ABCList=["C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0","C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1","C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5"]
-PercList=["C2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5"]
+PercList=["0#","1#","2#","3#","4#","5#","6#","7#","8#","9#","A#","B#","C#","D#","E#","F#"]
 
 
 
@@ -262,13 +262,9 @@ for n in range(TotalPatterns):
 					if i>0:				#append duration value only if this isn't the first note in the list
 						NoiseNoteDuration=i-NoiseLastFrame
 						NoisePattern[n].append(NoiseNoteDuration+DurationModifier)
+		
 						
-					if NoiseNote=="D#":
-						NoisePattern[n].append(151)
-					if NoiseNote=="8#":
-						NoisePattern[n].append(236)
-						
-					#NoisePattern[n].append(NoiseNote)
+					NoisePattern[n].append(NoiseNote)
 					NoiseLastFrame=i
 					
 				if (NoiseNote==StopNote1) or (NoiseNote==StopNote2):
@@ -322,6 +318,7 @@ for n in range(TotalPatterns):
 	TriangleEndedOnD00=0
 	NoiseEndedOnD00=0
 	
+print "NoisePattern is",NoisePattern
 	
 #############################################	
 ######## Build out note data for VIC ########
@@ -490,18 +487,14 @@ for n in range(TotalOrders):
 	#print "Current Section is ",n,", loading in Pattern number ",CurrentOrder
 	CurrentPatternLen=len(CurrentPattern)
 	#print "CurrentPatternLen is ",CurrentPatternLen
-	
 	for i in range(CurrentPatternLen/2):
 		CurrentNoteEng=CurrentPattern[i*2]
-		
-		
 		CurrentDuration=CurrentPattern[i*2+1]
-		#print "CurrentNoteEng for Noise is",CurrentNoteEng," and duration is ",CurrentDuration
+		print "CurrentNoteEng for Noise is",CurrentNoteEng," and duration is ",CurrentDuration
 		
 		if ((CurrentNoteEng != 75) and (CurrentNoteEng != 80)):  #Normal note positions
-			#CurrentNotePos=PercList.index(CurrentNoteEng)
-			#Tone=PercTone[CurrentNotePos]
-			Tone=CurrentNoteEng
+			CurrentNotePos=PercList.index(CurrentNoteEng)
+			Tone=CurrentNotePos*6+128
 			buildstr=bytestr + str(Tone) + "; N4 Low Byte Data"
 			program_data_out.append(buildstr)
 			buildstr=bytestr + str(CurrentDuration) + "; N4 Duration Data"
